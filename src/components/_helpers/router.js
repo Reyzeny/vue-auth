21f3432cb0d488845bc6e85ee949fedcc772c845
pelumzeny <pelumzeny10@gmail.com>
2019-01-21 06:53:03 +0100

    WIP

--- src/components/_helpers/router.js
+++ src/components/_helpers/router.js
@@ -20,7 +20,7 @@ export const router = new Router({
 
 router.beforeEach((to, from, next) => {
     // to and from are both route objects. must call `next`.
-    const publicPages = ['/login', '/Register']
+    const publicPages = ['/login', '/register']
     const authRequired = !publicPages.includes(to.path)
     const loggedIn = localStorage.getItem('user')
 
